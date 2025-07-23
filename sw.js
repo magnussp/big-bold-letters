c0f9d3d2ebea4152a68b537e2b80baeda902c650
Magnus Spånggård <magnus@detecht.se>
2025-07-23 09:55:49 +0200

    pwa

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'boldletters-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install event - cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event - serve cached or fetch from network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
